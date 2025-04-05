8e8d6d0b0005e770921e6bf6c6d4b917ef82d598
Patrick Raedler <code@readpato.dev>
2025-04-05 22:11:13 +0200

    chore(.weztermlua): tweak config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,18 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "tokyonight_moon"
 config.font = wezterm.font("Berkeley Mono")
 -- work
-config.font_size = 14
+config.font_size = 18
 -- home
 -- config.font_size = 13.6
 -- config.font_size = 13.2
-config.line_height = 1.3
+config.line_height = 1.4
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 return config
